75e29d70b2cd3d4740af017e4fc0398b6de8e9eb
alexeybersh <balexworks@gmail.com>
2023-10-20 13:30:34 +0300

    ПР12

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/react-mesto-auth",
   plugins: [react()],
   server: { open: true, port: 5173, },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
-  },
 })
